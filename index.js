9634ad91bb2b6726ed0366d30b0c1e34c210626d
Sanjaykannan24 <sanjaykannan389@gmail.com>
2021-10-01 13:42:51 -0400

    index file added

---
+++ index.js
@@ -0,0 +1,49 @@
+let express = require('express');
+let express_app = express();
+
+//http://localhost:8088/
+express_app.get("/" , (req , res) => {
+    res.send("<h1>Name : Sanjay Kannan</h1>" + 
+    "<h2>Student ID : 101232944</h2>" + 
+    "<h4>This is Lab Exercise 04</h4>");
+})
+
+//http://localhost:8088/hello
+express_app.get("/hello" , (req , res) => {
+    res.send("<h1>Hello Express JS</h1>")
+})
+
+//Querystring
+//http://localhost:8088/user?firstname=Sanjay&lastname=Kannan
+express_app.get("/user" , (req , res) => {
+    let firstname = req.query.firstname;
+    let lastname = req.query.lastname;
+
+    output = {
+        status: "Success",
+        firstname,
+        lastname
+    }
+    res.send(output);
+})
+
+//Post
+//Querystring
+//http://localhost:8088/user?firstname=Sanjay&lastname=Kannan
+express_app.post("/user" , (req , res) => {
+    let firstname = req.query.firstname;
+    let lastname = req.query.lastname;
+
+    output = {
+        status: "Success",
+        firstname,
+        lastname
+    }
+    res.send(output);
+})
+
+let server = express_app.listen(8088 , () => {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("Server running at http://%s:%s" , host , port);
+})
